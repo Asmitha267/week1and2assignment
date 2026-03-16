0506c2d70862c8a6e9f2eb57e199ee1256a94d02
afdal_vc <afdalr903@gmail.com>
2026-03-16 16:11:24 +0530

    UC2

--- BookMyStayApp/src/BookMyStayApp.java
+++ BookMyStayApp/src/BookMyStayApp.java
@@ -1,9 +1,46 @@
+/**
+ * UseCase2RoomInitialization
+ * Initializes room inventory for Book My Stay application
+ *
+ * @version 2.0
+ */
+
+class Room {
+
+    int roomNumber;
+    String roomType;
+    boolean isAvailable;
+
+    Room(int roomNumber, String roomType, boolean isAvailable) {
+        this.roomNumber = roomNumber;
+        this.roomType = roomType;
+        this.isAvailable = isAvailable;
+    }
+
+    void displayRoom() {
+        System.out.println("Room Number: " + roomNumber);
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Available: " + isAvailable);
+    }
+}
+
 public class BookMyStayApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Book My Stay");
-        System.out.println("Hotel Booking System v1.0");
+        System.out.println("Book My Stay - Hotel Booking System v2.0");
 
+        Room[] rooms = new Room[3];
+
+        rooms[0] = new Room(101, "Single", true);
+        rooms[1] = new Room(102, "Double", true);
+        rooms[2] = new Room(103, "Suite", false);
+
+        System.out.println("\nRoom Inventory:");
+
+        for (Room room : rooms) {
+            room.displayRoom();
+            System.out.println();
+        }
     }
 }
